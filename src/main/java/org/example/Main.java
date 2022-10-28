6fe206974b8c9b6f5a118af16cceb1516096dee3
Keyurx11 <keyurx11@gmail.com>
2022-10-28 09:22:41 +0100

    Initial commit - set up pom.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.*;
+
+public class Main {
+    private static Scanner stdin = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+    }
+}
