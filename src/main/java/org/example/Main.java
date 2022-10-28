e6b3b2eb99a917ce33bbde6c83fcb8189251ab35
Keyurx11 <keyurx11@gmail.com>
2022-10-28 10:01:52 +0100

    Set up connection string. Created properties file. Added properties to gitignore.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,25 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 
 public class Main {
     private static Scanner stdin = new Scanner(System.in);
 
     public static void main(String[] args) {
+        String connectionString = "jdbc:postgresql://localhost:5432/University";
 
+        Properties props = new Properties();
+
+        try{
+            InputStream input = Main.class.getClassLoader().getResourceAsStream("db.properties");
+            props.load(input);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return;
+        }
     }
 }
